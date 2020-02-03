c840f70509d6264ec9ba034e8f2fbd67963b225e
Connor Claxton <czclaxton@gmail.com>
2020-02-03 17:32:24 -0600

    cooldown and move commands

--- src/commands.js
+++ src/commands.js
@@ -9,11 +9,26 @@ const coolDown = () => {
     if (coolDownTime > 1) {
       setTimeout(() => {
         resolve(true);
-      });
+      }, +coolDownTime * 1000);
+    } else {
+      resolve(true);
     }
   });
 };
 
 export const move = async (direction, nextRoomId) => {
   await coolDown;
+
+  return axios
+    .post(`${config.API_PATH}/move`, {
+      direction: direction,
+      next_room_id: nextRoomId
+    })
+    .then(({ data }) => {
+      coolDownTime = data.cooldown ? +data.cooldown : 15;
+      return data;
+    })
+    .catch(err => {
+      throw err;
+    });
 };

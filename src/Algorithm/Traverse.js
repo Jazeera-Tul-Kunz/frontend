99ba30efc80aeb29dd4e901810dbc05b4aac517b
Amir Yunas <ayunas@gmail.com>
2020-02-05 10:34:50 -0600

    graph is being generated by randomly choosing an exit to explore and move to. will now try to refactor logic all into the Traverse Data Structure and use a nonrandom traversal selection process.

--- src/Algorithm/Traverse.js
+++ src/Algorithm/Traverse.js
@@ -17,6 +17,7 @@ class Traverse {
             const room = res.data;
             // console.log('room', room);
             this.graph[room.room_id] = [room.cooldown,...room.exits];
+            // this.graph[room.room_id] = room;
             console.log('graph', this.graph);
             // fs.writeFile('./rooms.json',this.graph[room.room_id], err => {if (err) throw err})
             // const stream = fs.createWriteStream("./rooms.js", {flags : 'a'});
@@ -32,20 +33,30 @@ class Traverse {
             const res = await axiosAuth(token).post('/move',{direction : way})
             const room = res.data;
             this.graph[room.room_id] = [room.cooldown,...room.exits];
+            // this.graph[room.room_id] = room;
             // const stream = fs.createWriteStream("./rooms.js", {flags : 'a'});
             // stream.write(this.graph[room.room_id], err => {if (err) throw err})
             // console.log('graph', this.graph);
-            fs.writeFile('./rooms.json',this.graph, err => {if (err) throw err })
+            // fs.writeFile('./rooms.json',this.graph, err => {if (err) throw err })
             // stream.end()
-            
+            console.log('graph in explore', this.graph)
             return [room.cooldown, room.exits]
         } catch(err) {
+            // if (err.response) {
             console.log(err.response.data);
+            // }
         }
     }
 }
 
 const t = new Traverse()
+
+const wait = seconds => {
+    console.log('seconds', seconds);
+    let makeMS = seconds * 1000;
+    return new Promise((resolve, reject) => setTimeout(() => resolve('time to move!'),makeMS));
+  };
+
 
 const begin = async () => {
         const [cooldown,exits] = await t.start();
@@ -55,28 +66,69 @@ const begin = async () => {
 
 const traverse = ([cooldown,way]) => {
     console.log('cooldown','way',cooldown,way);
-    const promise = new Promise((resolve,reject) => {
-        setTimeout(async () => {
-            const [cool,exits] = await t.explore(way)
+    try {
+        return wait(cooldown).then( async (res) => {
+            console.log(res);
+            const [cool,exits] = await t.explore(way);
+            console.log('coolexits in traverse', cool,exits)
+            // const explored = await t.explore(way);
+            // console.log('explored', explored);
             const rand = Math.floor(Math.random()*exits.length);
-            resolve([cool,exits[rand]])
-        },cooldown*1000)
-    })
-
-    return promise;
+            // return [cool,exits[rand]];
+            return [cool,exits[rand]];
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
 
 async function wrap() {
     let coolexit = await begin();
-    // const coolexit2 = await traverse(coolexit1)
+    // console.log('coolexit from begin()',coolexit);
+    // const coolexit2 = await traverse(coolexit);
+    // console.log('coolexit2', coolexit2);
+    // const ce3 = await traverse(coolexit2);
+    // console.log('ce3', ce3);
+
     for (let i=0; i<10; i++) {
-       const nextCE = await traverse(coolexit);
-       coolexit = nextCE;
+        const nextCE = await traverse(coolexit);
+        coolexit = nextCE;
+    //     try {
+    //         
+    //         console.log('nextCE',nextCE);
+    //         console.log('coolexit', coolexit);
+    //         coolexit = nextCE;
+    //     }
+    //     catch(err) {
+    //         console.log('err', err)
+    //     }
     }
 }
 wrap();
 
 
+ // .then(() => {
+    //     t.explore(way)
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err))
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // })
+
+    // const promise = new Promise((resolve,reject) => {
+    //     setTimeout(async () => {
+    //         try {
+    //             const [cool,exits] = await t.explore(way)
+    //             const rand = Math.floor(Math.random()*exits.length);
+    //             resolve([cool,exits[rand]])
+    //         } catch(err) {
+    //             console.log('t.explore() err',err)
+    //         }
+            
+    //     },cooldown*1000)
+    // })
 
 
 
